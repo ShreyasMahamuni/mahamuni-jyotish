be32119e75091c9271045520a802e30d43855f67
Shreyas Mahamuni <shreyasmahamuni9@gmail.com>
2025-07-13 23:28:51 +0530

    model migrations

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const { connectDB } = require("./confitg/db");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+require("dotenv").config({
+  path: ".env",
+  encoding: "utf8",
+  quiet: true,
+});
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("<h4>WELCOME TO MAHAMUNI JYOTISH</h4>");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+
+async function startServer() {
+  const isConnect = await connectDB();
+  if (!isConnect) {
+    console.error("Fatal: Database connection failed. Exiting...");
+    process.exit(1);
+  } else {
+    app.listen(PORT, () => {
+      console.log(`Server is running on ${PORT}`);
+    });
+  }
+}
+
+startServer();
